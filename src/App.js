ea1f586381361a3bb778e19730e6e0c335f2cf60
kingstech5566 <kingsleychijioke@gmail.com>
2022-10-28 19:10:41 +0100

    feat: New file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';   
+
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>   
+<Avatar image="kingsley.jpg" size="xlarge">
+    <Badge value="4" severity="danger" />
+</Avatar>
+<p>Kingsley Chijioke</p>
     </div>
   );
 }
