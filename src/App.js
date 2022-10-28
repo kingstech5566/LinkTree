78afd068e47f6b183d600ed1426206ca8c3cce3d
kingstech5566 <kingsleychijioke@gmail.com>
2022-10-28 21:21:38 +0100

    feat: Linktree

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import 'primeflex/primeflex.css';   
 
-import { Avatar } from 'primereact/avatar';
-import { Badge } from 'primereact/badge';
+import Profile from "./Components/Proflie";
+import Links from "./Components/Links";
 
 function App() {
   return (
-    <div>   
-<Avatar image="kingsley.jpg" size="xlarge">
-    <Badge value="4" severity="danger" />
-</Avatar>
-<p>Kingsley Chijioke</p>
+    <div className="p-m-4">
+     <Profile />
+     <Links />
     </div>
   );
 }
---
+++ src/Components/Proflie.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+
+function Profile() {
+  return (
+    <div>
+    <div className="p-d-flex p-jc-center p-ai-center">
+      <Avatar image="kingsley.jpg" size="xlarge" shape="circle">
+        <Badge value="4" severity="info" />
+        </Avatar>
+        </div>
+        <div className="p-d-flex p-jc-center">
+          <p>Kingsley Chijioke</p>
+          </div>
+          </div>
+  );
+}
+
+export default Profile;
